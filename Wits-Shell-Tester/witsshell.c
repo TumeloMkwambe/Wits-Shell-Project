60dd5d38650bc83aae517d1ccda25272e06736d7
TumeloMkwambe <2446873@students.wits.ac.za>
2024-09-06 12:03:39 +0000

    Fix path bug

--- Wits-Shell-Tester/witsshell.c
+++ Wits-Shell-Tester/witsshell.c
@@ -14,20 +14,21 @@ void error(){
     write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
-void execute(char *line){
-    char path[INPUT_MAX] = "/bin/";
+void execute(char *line, char *path[]){
+    char default_path[INPUT_MAX] = "/bin/";
     char *args[INPUT_MAX] = {NULL};
+    
     int i = 0;
-
-    while((args[i] = strsep(&line, " ")) != NULL){
+    while((args[i] = strsep(&line, " ")) != NULL){ // parses line and adds arguments to args
         if(strlen(args[i]) > 0){
             i++;
         }
+        args[i] = NULL;
     }
 
     int k = 0;
     int n = sizeof(args) / sizeof(args[0]);
-    if(strcmp(args[0], "cd") == 0){
+    if(strcmp(args[0], "cd") == 0){ // checks if cd command and then checks argument count
         for(int j = 1; j < n; j++){
             if(args[j] != NULL){
                 k++;
@@ -36,16 +37,30 @@ void execute(char *line){
         if(k != 1){
             error();
         }
-        chdir(args[1]);
+        chdir(args[1]); // if argument count = 1, then change directory
     }
-    else if(strcmp(args[0], "exit") == 0){
+    else if(strcmp(args[0], "exit") == 0){ // if exit command with argument, then throw error
         error();
     }
+    else if(strcmp(args[0], "path") == 0){
+        if(args[1] == NULL){
+            path[0] = "/bin/";
+        }
+        else{
+            int i = 0;
+            while(args[i] != NULL){
+                path[i] = args[i+1];
+                i++;
+            }
+        }
+    }
     else{
-        strcat(path, args[0]);
+        printf("Path[0] = %s\n", path[0]); // BUG: Arguments to command erase value of path[0]!
+
+        strcat(default_path, args[0]);
         int pid = fork();
         if(pid == 0){
-            int status = execv(path, args);
+            int status = execv(default_path, args);
             if(status == -1){
                 error();
             }
@@ -57,6 +72,7 @@ void execute(char *line){
 }
 
 int main(int MainArgc, char *MainArgv[]){
+    char *path[INPUT_MAX] = {NULL};
     FILE *file;
     char *line = NULL;
     size_t len = 0;
@@ -75,7 +91,7 @@ int main(int MainArgc, char *MainArgv[]){
             if(strcmp(line, "exit") == 0){
                 exit(0);
             }
-            execute(line);
+            execute(line, path);
         }
 
         return(0);
@@ -92,13 +108,7 @@ int main(int MainArgc, char *MainArgv[]){
             exit(0);
         }
 
-        int pid = fork();
-        if(pid == 0){
-            execute(line);
-        }
-        else{
-            wait(NULL);
-        }
+        execute(line, path);
     }
 
     return(0);
