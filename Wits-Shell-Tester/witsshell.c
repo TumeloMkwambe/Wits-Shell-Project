8fc947e89b225d5d68d333434693a33302933cf9
TumeloMkwambe <2446873@students.wits.ac.za>
2024-09-08 10:39:06 +0000

    Restructure Complete!

--- Wits-Shell-Tester/witsshell.c
+++ Wits-Shell-Tester/witsshell.c
@@ -9,13 +9,111 @@
 
 #define INPUT_MAX 10
 
+char *path[INPUT_MAX] = {"/bin/", "/usr/bin/", NULL};
+
 void error(){
     char error_message[30] = "An error has occurred\n";
     write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
+void exit_handler(char *args[], int n){
+    int i = 0;
+    for(int j = 1; j < n; j++){ // count arguments of exit command
+        if(args[j] != NULL){
+            i++;
+        }
+    }
+    if(i > 0){ // if exit has atleast 1 argument, throw error
+        error();
+    }
+    exit(0);
+}
+
+void cd_handler(char *args[], int n){
+    int i = 0;
+    for(int j = 1; j < n; j++){ // count arguments of cd command
+        if(args[j] != NULL){
+            i++;
+        }
+    }
+    if(i != 1){ // if cd has less or more than 1 argument, throw error
+        error();
+    }
+    chdir(args[1]); // change directory in main process if cd has 1 command
+}
+
+void path_handler(char *args[], int n){
+    int i = 0;
+    for(int j = 1; j < n; j++){ // count arguments of path command
+        if(args[j] != NULL){
+            i++;
+        }
+    }
+    if(i == 0){ // if the path is empty
+        path[0] = NULL;
+        path[1] = NULL;
+    }
+
+}
+
 void built_in_command(char *args[], int n){
-    printf("Built-in command!\n");
+    int i = 0;
+    if(strcmp(args[0], "exit") == 0){ // if built-in command is exit
+        exit_handler(args, n);
+    }
+    else if(strcmp(args[0], "cd") == 0){ // if built-in command is cd
+        cd_handler(args, n);
+    }
+    else if(strcmp(args[0], "path") == 0){ // if built-in command is path
+        path_handler(args, n);
+    }
+}
+
+void execute(char *args[], char *path_variable){
+    int pid = fork();
+        if(pid == 0){
+            execv(path_variable, args);
+        }
+        else{
+            wait(NULL);
+        }
+}
+
+void execute_command(char *args[]){
+    if(path[0] == NULL){
+        error();
+    }
+    else{
+        int n = sizeof(path) / sizeof(path[0]);
+        for(int j = 0; j < n; j++){ // travers path array, checks for executable and execute if found
+            if(path[j] != NULL){
+                char path_variable[1024];
+                strcpy(path_variable, path[j]);
+                strcat(path_variable, args[0]);
+                int path_stat = access(path_variable, X_OK);
+                if(path_stat == 0){
+                    execute(args, path_variable);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+int check_shell_script(char *command){
+    const char *extension = ".sh";
+    size_t len_filename = strlen(command);
+    size_t len_extension = strlen(extension);
+
+    if (len_filename < len_extension) {
+        return 1;
+    }
+
+    if (strcmp(command + len_filename - len_extension, extension) == 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 void parse_command(char *line){
@@ -25,7 +123,6 @@ void parse_command(char *line){
     // Separate command arguments and adds them to args array!
     while((args[i] = strsep(&line, " ")) != NULL){
         if(strlen(args[i]) > 0){
-            printf("args[%d] = %s\n", i, args[i]);
             i++;
         }
     }
@@ -35,16 +132,14 @@ void parse_command(char *line){
         int n = sizeof(args) / sizeof(args[0]);
         built_in_command(args, n);
     }
-}
 
-void execute(char *command, char *args[]){
-    int pid = fork();
-    if(pid == 0){
-        execv(command, args);
+    // Check shell command
+    if(check_shell_script(args[0]) == 0){
+        error();
     }
-    else{
-        wait(NULL);
-    }
+
+    // execute command
+    execute_command(args);
 }
 
 int main(int MainArgc, char *MainArgv[]){ 
