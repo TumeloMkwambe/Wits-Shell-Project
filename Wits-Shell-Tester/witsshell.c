2e4c9f19d10f12e7465f36dbff6b238697514932
TumeloMkwambe <2446873@students.wits.ac.za>
2024-09-11 18:55:16 +0000

    Redirection Complete

--- Wits-Shell-Tester/witsshell.c
+++ Wits-Shell-Tester/witsshell.c
@@ -16,7 +16,7 @@ void error(){
     write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
-void exit_handler(char *args[], int n){
+void exitHandler(char *args[], int n){
     int i = 0;
     for(int j = 1; j < n; j++){ // count arguments of exit command
         if(args[j] != NULL){
@@ -29,7 +29,7 @@ void exit_handler(char *args[], int n){
     exit(0);
 }
 
-void cd_handler(char *args[], int n){
+void cdHandler(char *args[], int n){
     int i = 0;
     for(int j = 1; j < n; j++){ // count arguments of cd command
         if(args[j] != NULL){
@@ -42,7 +42,7 @@ void cd_handler(char *args[], int n){
     chdir(args[1]); // change directory in main process if cd has 1 command
 }
 
-void path_handler(char *args[], int n){
+void pathHandler(char *args[], int n){
     int i = 0;
     
     for(int j = 1; j < n; j++){ // count arguments of path command
@@ -63,17 +63,53 @@ void path_handler(char *args[], int n){
     }
 }
 
-void built_in_command(char *args[], int n){
+void builtInCommand(char *args[], int n){
     int i = 0;
     if(strcmp(args[0], "exit") == 0){ // if built-in command is exit
-        exit_handler(args, n);
+        exitHandler(args, n);
     }
     else if(strcmp(args[0], "cd") == 0){ // if built-in command is cd
-        cd_handler(args, n);
+        cdHandler(args, n);
     }
     else if(strcmp(args[0], "path") == 0){ // if built-in command is path
-        path_handler(args, n);
+        pathHandler(args, n);
     }
+}
+
+int redirectionParser(char *args[], int argc){
+    for(int i = 0; i < argc; i++){ // traverse arguments array
+        if(i == 0 && strlen(args[i]) == 1){ // if there is no command before >
+            error();
+            return -1;
+        }
+        else if(strstr(args[i], ">") != NULL && strlen(args[i]) != 1){ // if args element contains >
+            char* token = strtok(args[i], ">");
+            while (token != NULL){
+                args[i] = token;
+                //printf("args[%d] = %s\n", i, args[i]);
+                if(strlen(args[i]) > 0){
+                    i++;
+                }
+                token = strtok(NULL, ">");
+            }
+        }
+        else if(strstr(args[i], ">") != NULL){
+            if(args[i+1] == NULL || args[i+2]){ // if there is not output file or more than 1 output file
+                error();
+                return -1;
+            }
+        }
+    }
+
+    int count = 0;
+    while(args[count] != NULL){
+        count++;
+    }
+    if(count < 2){
+        error();
+        return -1;
+    }
+    return 0;
 }
 
 void execute(char *args[], char *path_variable){
@@ -86,7 +122,7 @@ void execute(char *args[], char *path_variable){
         }
 }
 
-void execute_command(char *args[]){
+void executeCommand(char *args[]){
     if(path[0] == NULL){
         error();
     }
@@ -107,7 +143,25 @@ void execute_command(char *args[]){
     }
 }
 
-int check_shell_script(char *command){
+void redirectionHandler(char *args[]){
+    char filename[1024];
+    //char *updated_args[INPUT_MAX] = {NULL};
+    int i = 0;
+    while(args[i] != NULL){
+        i++;
+    }
+    strcpy(filename, args[i-1]);
+    args[i-1] = NULL;
+    FILE *file = freopen(args[i+1], "w", stdout); // reroutes stdout for main process
+    dup2(fileno(stdout), fileno(stderr));
+    executeCommand(args);
+    fclose(file);
+    freopen("/dev/tty", "w", stdout); // reroutes standard output to terminal
+    freopen("/dev/tty", "w", stderr); // reroutes standard error to terminal
+    
+}
+
+int checkShellScript(char *command){
     const char *extension = ".sh";
     size_t len_filename = strlen(command);
     size_t len_extension = strlen(extension);
@@ -123,31 +177,48 @@ int check_shell_script(char *command){
     return 1;
 }
 
-void parse_command(char *line){
+void parseCommand(char line[]){
     char *args[INPUT_MAX] = {NULL};
-    
     int i = 0;
-    // Separate command arguments and adds them to args array!
-    while((args[i] = strsep(&line, " ")) != NULL){
-        if(strlen(args[i]) > 0){
-            i++;
+
+    if(strstr(line, ">") != NULL){ // if > is present in input
+        while((args[i] = strsep(&line, " ")) != NULL){
+            if(strlen(args[i]) > 0){
+                i++;
+            }
         }
+        if(redirectionParser(args, i) == 0){
+            redirectionHandler(args);
+        }
+        
     }
-
-    // Handle built-in commands
-    if(strcmp(args[0], "exit") == 0 || strcmp(args[0], "cd") == 0 || strcmp(args[0], "path") == 0){
-        int n = sizeof(args) / sizeof(args[0]);
-        built_in_command(args, n);
-    }
-
-    // Check shell command
-    else if(check_shell_script(args[0]) == 0 && (path[0] == NULL || strcmp(path[0], "/bin/") == 0)){
-        error();
-    }
-
-    // execute command
     else{
-        execute_command(args);
+        // Separate command arguments and adds them to args array!
+        while((args[i] = strsep(&line, " ")) != NULL){
+            if(strlen(args[i]) > 0){
+                i++;
+            }
+        }
+
+        if(i == 0){
+            // whitespaces
+        }
+
+        // Handle built-in commands
+        else if(strcmp(args[0], "exit") == 0 || strcmp(args[0], "cd") == 0 || strcmp(args[0], "path") == 0){
+            int n = sizeof(args) / sizeof(args[0]);
+            builtInCommand(args, n);
+        }
+
+        // Check shell command
+        else if(checkShellScript(args[0]) == 0 && (path[0] == NULL || strcmp(path[0], "/bin/") == 0)){
+            error();
+        }
+
+        // execute command
+        else{
+            executeCommand(args);
+        }
     }
 }
 
@@ -159,15 +230,24 @@ int main(int MainArgc, char *MainArgv[]){
 
     if(MainArgc > 2){ // Invalid argument count
         error();
+        exit(1);
     }
 
     else if(MainArgc == 2){ // Batch case
         file = fopen(MainArgv[1], "r");
-        while ((nread = getline(&line, &len, file)) != -1) {
-            if (line[nread - 1] == '\n') {
-                line[nread - 1] = '\0';
+        if(file != NULL){
+            while ((nread = getline(&line, &len, file)) != -1) {
+                if(strlen(line) > 1){
+                    if (line[nread - 1] == '\n') {
+                        line[nread - 1] = '\0';
+                    }
+                    parseCommand(line);
+                }
             }
-            parse_command(line);
+        }
+        else{
+            error();
+            exit(1);
         }
         //return(0);
     }
@@ -177,10 +257,12 @@ int main(int MainArgc, char *MainArgv[]){
         {
             printf("witsshell> ");
             nread = getline(&line, &len, stdin);
-            if (line[nread - 1] == '\n') {
-                line[nread - 1] = '\0';
+            if(strlen(line) > 1){
+                if (line[nread - 1] == '\n') {
+                    line[nread - 1] = '\0';
+                }
+                parseCommand(line);
             }
-            parse_command(line);
         }
     }
 
